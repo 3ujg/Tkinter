2af2719791b6764cfbb395317cbadba5332e2c14
Alar <alar@hkhk.edu.ee>
2025-02-04 11:24:15 +0200

    x<c

--- tk01.py
+++ tk01.py
@@ -10,17 +10,30 @@ def kuva_pilt(aken, failinimi, laius, korgus):
     label.image = foto
     label.pack()
 
+def loe_fail(failinimi):
+    with open(failinimi, 'r', encoding='utf-8') as file:
+        return file.read()
+
 # Peafunktsioon, mis loob Tkinteri akna ja lisab sinna sildi ja nupu
 def main():
     aken = tk.Tk()
     aken.title("Mario ülesanded")
     aken.geometry("400x300")
     aken.resizable(True, False)
-   
+   # silt
     label = tk.Label(aken, text="Chuck Norris", font=("Arial",16,"bold"), fg="blue").pack()
+   # pilt
     kuva_pilt(aken, "chekes.jpg", 200, 200)
-    aken.mainloop()
+   # tekst
+    tekst = tk.Text(aken, wrap=tk.WORD, font=("Helvetica", 12, "bold"), fg="blue")
+    scrollbar = tk.Scrollbar(aken, command=tekst.yview)
+    tekst.config(yscrollcommand=scrollbar.set)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    tekst.pack(expand=True, fill=tk.BOTH)
 
+    failisisu = loe_fail("text.txt")
+    tekst.insert(tk.INSERT, failisisu)
+    aken.mainloop()
 # Käivitame peafunktsiooni
 if __name__ == "__main__":
     main()
