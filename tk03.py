e9aed15b86157c2f150d3ec6417e576dad290d5e
Alar <alar@hkhk.edu.ee>
2025-02-05 13:05:01 +0200

    jhibb

--- tk03.py
+++ tk03.py
@@ -6,24 +6,27 @@ def main():
 
     # Funktsioon, mis kuvab sisestused
     def kuva_sisestus():
-        tekst1 = sisestus1.get()  
-        tekst2 = sisestus2.get() 
-        tekst3 = sisestus3.get() 
-        vastus = tk.Label(aken, text=f"Esimene sisestus: {tekst1}, Teine sisestus: {tekst2}, Kolmas sisestus: {tekst3}")
+        laenusumma = int(sisestus1.get())  
+        kuuintressimäär = float(sisestus2.get())/12 
+        maksete_arv = int(sisestus3.get())*12 
+
+        igakuine_makse = laenusumma * kuuintressimäär / (1 - (1 + kuuintressimäär)** -maksete_arv)
+        
+        vastus = tk.Label(aken, text=f"Igakuine makse: {maksete_arv:.2f}")
         vastus.pack()
 
     # Esimene sisestusväli
-    label = tk.Label(aken, text="Chuck Norris", font=("Arial",16,"bold"), fg="blue").pack()
+    label = tk.Label(aken, text="Laenusumma", font=("Arial",16,"bold"), fg="blue").pack()
     sisestus1 = tk.Entry(aken)
     sisestus1.pack()
    
     # Teine sisestusväli
-    label = tk.Label(aken, text="Chuck Norris", font=("Arial",16,"bold"), fg="blue").pack()
+    label = tk.Label(aken, text="Aastane intressimäär (%)", font=("Arial",16,"bold"), fg="blue").pack()
     sisestus2 = tk.Entry(aken)
     sisestus2.pack()
    
     # Kolmas sisestusväli
-    label = tk.Label(aken, text="Chuck Norris", font=("Arial",16,"bold"), fg="blue").pack()
+    label = tk.Label(aken, text="Laenuperiood (Aastates)", font=("Arial",16,"bold"), fg="blue").pack()
     sisestus3 = tk.Entry(aken)
     sisestus3.pack()
 
