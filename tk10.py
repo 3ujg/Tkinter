b2138333c6ddc4baafb73e9bcee83a7981ef39ee
Alar <alar@hkhk.edu.ee>
2025-05-21 09:22:06 +0300

    smth

--- tk10.py
+++ tk10.py
@@ -1,18 +1,10 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
+import tkinter as tk
 
 aken = ttk.Window(themename="darkly")
-aken.geometry("400x400")
 
-def show_selection():
-    print("Nupp on vajutatud!")
-
-btn_show = ttk.Button(aken, text="Näita valikut", command=show_selection, bootstyle="warning")
-btn_show.pack(pady=20)
-
-
-
-aken = ttk.Tk()
+aken = tk.Tk()
 aken.title("Raadionuppude näidis")
 aken.geometry("300x400")
 
@@ -33,18 +25,9 @@ selected_color = tk.IntVar(value=5)
 
 
 
-ttk.Label(aken, text="Kasutaja ID", font="Arial,16").pack(pady=(10, 0))
-ttk.Entry(aken).pack()
+tk.Label(aken, text="Kasutaja ID", font="Arial,16", justify="left").pack(pady=(10, 0))
+tk.Entry(aken).pack()
 
-
-
-# meetod uue akna avamiseks
-def open_new_window():
-    new_window = tk.Toplevel()
-    new_window.title("Uus aken")
-    new_window.geometry("200x300")
-    ttk.Label(new_window, text="See on uus aken").pack()
-    ttk.Button(new_window, text="Sulge aken", command=new_window.destroy).pack()
 
 
 
@@ -52,11 +35,11 @@ def show_selection():
     print("Valitud värv:", selected_color.get())
 
 #StringVar, mis hoiab valitud värvi nime
-selected_color = ttk.StringVar(value=5)
+selected_color = tk.StringVar(value=5)
 
 
 #Loome raadionupud
-ttk.Label(aken, text="Vali pitsa suurus", font="Arial 14").pack()
+tk.Label(aken, text="Vali pitsa suurus", font="Arial 14").pack()
 radio_red = tk.Radiobutton(aken,text="Väike (5€)", variable=selected_color, value=5)
 radio_green = tk.Radiobutton(aken,text="Suur (8€)", variable=selected_color, value=8)
 radio_blue = tk.Radiobutton(aken,text="Pere (12€)", variable=selected_color, value=12)
@@ -64,31 +47,30 @@ radio_red.pack()
 radio_green.pack()
 radio_blue.pack()
 
-var1 = ttk.IntVar(value=0)
-var2 = ttk.StringVar(value=0)
-var3 = ttk.IntVar(value=0)
-ttk.Label(aken, text="Vali lisandid", font="Arial 14").pack()
+var1 = tk.IntVar(value=0)
+var2 = tk.StringVar(value=0)
+var3 = tk.IntVar(value=0)
+tk.Label(aken, text="Vali lisandid", font="Arial 14").pack()
 
 # Checkbox
-checkbox1 = ttk.Checkbutton(aken, text="Juust (+1€)", variable=var1, onvalue=1, offvalue=0)
-checkbox2 = ttk.Checkbutton(aken, text="Pepperoni (+1.5€)", variable=var2, onvalue=1.5, offvalue=0)
-checkbox3 = ttk.Checkbutton(aken, text="Seen (+1€)", variable=var3, onvalue=1, offvalue=0)
+checkbox1 = tk.Checkbutton(aken, text="Juust (+1€)", variable=var1, onvalue=1, offvalue=0)
+checkbox2 = tk.Checkbutton(aken, text="Pepperoni (+1.5€)", variable=var2, onvalue=1.5, offvalue=0)
+checkbox3 = tk.Checkbutton(aken, text="Seen (+1€)", variable=var3, onvalue=1, offvalue=0)
 checkbox1.pack()
 checkbox2.pack()
 checkbox3.pack()
 
 
 # dropdown
-ttk.Label(aken, text="Vali transport", font="Arial 14").pack()
+tk.Label(aken, text="Vali transport", font="Arial 14").pack()
 valikud = ["Kuller", "Kohapeal"]
-selected_option = ttk.StringVar()
+selected_option = tk.StringVar()
 selected_option.set("Vali üksus")
 
-dropdown = ttk.OptionMenu(aken, selected_option, *valikud)
+dropdown = tk.OptionMenu(aken, selected_option, *valikud)
 dropdown.pack()
 
-btn_confirm = ttk.Button(aken, text="Kinnita valik", command=show_selection)
+btn_confirm = tk.Button(aken, text="Kinnita valik", command=show_selection)
 btn_confirm.pack()
-
 
 aken.mainloop()
